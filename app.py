defdb5ca4db7675c9effea0f48b59f1f9a43690d
Della Agustin <163804538+delladeldel@users.noreply.github.com>
2026-09-10 00:38:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 import joblib
 from datetime import timedelta
-import keras
+
 
 
 from sklearn.preprocessing import MinMaxScaler
